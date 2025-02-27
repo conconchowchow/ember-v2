b259bee759831848233a9b674d0099e244ed80e3
connorchow <connorchow2@gmail.com>
2025-02-26 19:13:19 -0800

    creating diversity operator

--- src/ember/core/registry/operator/core/diversity_scorer.py
+++ src/ember/core/registry/operator/core/diversity_scorer.py
@@ -8,15 +8,18 @@ from src.ember.core.registry.operator.base.operator_base import Operator
 
 from src.ember.core.registry.prompt_signature.signatures import Signature
 
+from src.ember.core.utils.eval.evaluators import DiversityScoringEvaluator
+
 class DiversityScoringOperatorInputs(BaseModel):
     responses: List[str]
 
+#TODO: fix
 class DiversityScoringOperatorOutputs(BaseModel):
     responses: List[str]
     diversity_score: int
 
-class DiversityScoringSignature(Signature):
-    pass
-
 class DiversityScoringOperator(Operator[DiversityScoringOperatorInputs, Dict[str, Any]]):
-    pass
+    def forward(self, *, inputs: DiversityScoringOperatorInputs) -> Dict[str, Any]:
+        responses: List[Any] = inputs.model_dump()['responses'] 
+        diversity_score: int = DiversityScoringEvaluator().evaluate()['score']
+        return {"responses": responses, "divserity_score": diversity_score}
